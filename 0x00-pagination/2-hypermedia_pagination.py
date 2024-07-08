cbd19bcfa665f230ef87b544c7743f551da51a47
muyenzi10 <muyenzija10@gmail.com>
2024-07-08 11:55:42 +0000

    up

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,17 +1,9 @@
 #!/usr/bin/env python3
-"""Task 1: Simple pagination.
 """
-
+Adds `get_hyper` method to `Server` class
+"""
 import csv
-import math
-from typing import List, Tuple
-
-
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """Retrieves the index range from a given page and page size.
-    """
-
-    return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
+from typing import Dict, List, Tuple, Union
 
 
 class Server:
@@ -33,13 +25,47 @@ class Server:
 
         return self.__dataset
 
+    @staticmethod
+    def index_range(page: int, page_size: int) -> Tuple[int, int]:
+        """Calculate start and end index range for a `page`, with `page_size`
+        """
+        nextPageStartIndex = page * page_size
+        return nextPageStartIndex - page_size, nextPageStartIndex
+
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Retrieves a page of data.
+        """
+        Get items for the given page number
+        Args:
+            page (int): page number
+            page_size (int): number of items per page
+        Returns:
+            (List[List]): a list of list(row) if inputs are within range
+            ([]) : an empty list if page and page_size are out of range
         """
         assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
-        start, end = index_range(page, page_size)
-        data = self.dataset()
-        if start > len(data):
-            return []
-        return data[start:end]
+        startIndex, endIndex = self.index_range(page, page_size)
+        return self.dataset()[startIndex:endIndex]
+
+    def get_hyper(self, page: int,
+                  page_size: int) -> Dict[str, Union[int, List[List], None]]:
+        """
+        Args:
+            page (int): page number
+            page_size (int): number of items per page
+        Returns:
+            A dictionary of the following:
+                * page_size, page, data, next_page, prev_page, total_pages
+        """
+        data = self.get_page(page, page_size)
+        totalRows = len(self.dataset())
+        prev_page = page - 1 if page > 1 else None
+        next_page = page + 1
+        if self.index_range(page, page_size)[1] >= totalRows:
+            next_page = None
+        total_pages = totalRows / page_size
+        if total_pages % 1 != 0:
+            total_pages += 1
+        return {'page_size': len(data), 'page': page,
+                'data': data, 'next_page': next_page,
+                'prev_page': prev_page, 'total_pages': int(total_pages)}
